8d0af94b755be1b7e4c0882b57bf6d63600a51d3
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-07 09:52:53 +0200

    apply a css class and remove the unused filter operator

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -2,22 +2,24 @@ import { Directive, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
 import { map, filter } from 'rxjs/operators';
 @Directive({
-  selector: '[appAnswerHighlight]'
+  selector: '[appAnswerHighlight]',
 })
 export class AnswerHighlightDirective {
+  constructor(private el: ElementRef, private controlName: NgControl) {}
 
-  constructor(private el: ElementRef, private controlName: NgControl) {
-    
-   }
-
-   ngOnInit() {
+  ngOnInit() {
     // console.log(this.controlName.control?.parent);
-    this.controlName.control?.parent?.valueChanges.pipe(
-      map(({a, b, answer}) => 
-        Math.abs((a + b - answer)/(a+b)))
-    ).subscribe(value => {
-      console.log(value);
-    })
-
-   }
+    this.controlName.control?.parent?.valueChanges
+      .pipe(
+        map(({ a, b, answer }) => Math.abs((a + b - answer) / (a + b)))        
+      )
+      .subscribe((value) => {        
+        if (value < 0.2) {
+          this.el.nativeElement.classList.add('close');
+        } else {
+          this.el.nativeElement.classList.remove('close');
+        }
+        console.log(value);
+      });
+  }
 }
